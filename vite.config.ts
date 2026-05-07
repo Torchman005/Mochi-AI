d1bb79cc2389e8d8f3726c8d16c8e277f9228d2c
Torchman005 <2318257072@qq.com>
2026-05-07 13:49:08 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron/simple'
+import renderer from 'vite-plugin-electron-renderer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: 'electron/main.ts',
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: 'electron/preload.ts',
+      },
+    }),
+    renderer(),
+  ],
+})
